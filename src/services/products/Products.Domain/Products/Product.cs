677c74990398846f0f1e1138b9809e8b5cdfa9aa
https://github.com/sophi1340 <sophi1340@gmail.com>
2024-06-18 01:51:39 +0430

    1403032901-Add-SeedData-DbContext-Product

--- src/services/products/Products.Domain/Products/Product.cs
+++ src/services/products/Products.Domain/Products/Product.cs
@@ -1,5 +1,8 @@
-﻿using Products.Domain.Base;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Products.Domain.Base;
 using Products.Domain.Categories;
+using System.Text.Json;
 
 namespace Products.Domain.Products
 {
@@ -16,5 +19,36 @@ namespace Products.Domain.Products
 
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+
+        //public class ProductConfiguration : IEntityTypeConfiguration<Product>
+        //{
+        //    public void Configure(EntityTypeBuilder<Product> builder)
+        //    {
+        //        builder.HasKey(b => b.Id);
+        //        builder.Property(p => p.Title).IsRequired().HasMaxLength(200);
+        //        builder.Property(p => p.Description).IsRequired().HasMaxLength(5000);
+        //        builder.Property(p => p.Permalink).IsRequired().HasMaxLength(100);
+        //        builder.Property(p => p.CoverImageUrl).IsRequired().HasMaxLength(50)
+        //            .HasDefaultValue("https://via.placeholder.com/150x150.png");
+        //        builder.Property(p => p.Code).IsRequired().HasMaxLength(50);
+        //        builder.Property(p => p.CreationDateTime).IsRequired().HasDefaultValue(DateTime.UtcNow);
+        //        builder.Property(p => p.ModificationDateTime).IsRequired().HasDefaultValue(DateTime.UtcNow);
+
+        //        builder.HasData(SeedProducts());
+        //    }
+        //    private List<Product> SeedProducts()
+        //    {
+        //        var products = new List<Product>();
+        //        string directoryPath = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+        //        string productSeedPath = Path.Combine(directoryPath, @"SeedData/ProductSeed.json");
+        //        using (StreamReader r = new StreamReader(productSeedPath))
+        //        {
+        //            string json = r.ReadToEnd();
+        //            products = JsonSerializer.Deserialize<List<Product>>(json);
+        //        }
+
+        //        return products ?? new();
+        //    }
+        //}
     }
 }
---
+++ src/services/products/Products.Infrastructure/ProductDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Domain.Categories;
+using Products.Domain.Products;
+
+namespace Products.Infrastructure
+{
+    internal class ProductDbContext: DbContext
+    {
+        public ProductDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Order is important. Because first we have insert into Category and then use categoryId when inserting Product
+            modelBuilder.ApplyConfiguration(new Category.CategoryConfiguration());
+            //modelBuilder.ApplyConfiguration(new Product.ProductConfiguration());
+        }
+    }
+}
