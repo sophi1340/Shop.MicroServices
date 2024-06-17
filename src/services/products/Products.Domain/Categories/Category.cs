ae7b60b3ae89d6550675c9328a5290858c1db08c
https://github.com/sophi1340 <sophi1340@gmail.com>
2024-06-17 16:51:00 +0430

    1403032802-Domain-Base

---
+++ src/services/products/Products.Domain/Categories/Category.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Products.Domain.Base;
+using Products.Domain.Products;
+using System.Text.Json;
+
+namespace Products.Domain.Categories
+{
+    public class Category : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int? ParentCategoryId { get; set; }
+        public string Permalink { get; set; }
+        public bool Active { get; set; }
+        public int Priority { get; set; }
+        public string BannerUrl { get; set; }
+        public string IconUrl { get; set; }
+        public string ThumbnailUrl { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+        public Category ParentCategory { get; set; }
+
+        public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+        {
+            public void Configure(EntityTypeBuilder<Category> builder)
+            {
+                builder.HasKey(b => b.Id);
+                builder.Property(p => p.Title).IsRequired().HasMaxLength(200);
+                builder.Property(p => p.Description).IsRequired().HasMaxLength(5000);
+                builder.Property(p => p.Permalink).IsRequired().HasMaxLength(200);
+                builder.Property(p => p.BannerUrl).IsRequired().HasMaxLength(50)
+                    .HasDefaultValue("https://via.placeholder.com/500x100.png");
+                builder.Property(p => p.IconUrl).IsRequired().HasMaxLength(50)
+                    .HasDefaultValue("https://via.placeholder.com/85.png");
+                builder.Property(p => p.ThumbnailUrl).IsRequired().HasMaxLength(50).HasDefaultValue("https://via.placeholder.com/150x150.png");
+                builder.Property(p => p.CreationDateTime).IsRequired().HasDefaultValue(DateTime.UtcNow);
+                builder.Property(p => p.ModificationDateTime).IsRequired().HasDefaultValue(DateTime.UtcNow);
+
+                builder.HasData(SeedCategories());
+            }
+
+            private List<Category> SeedCategories()
+            {
+                var categories = new List<Category>();
+                string directoryPath = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+                string categorySeedPath = Path.Combine(directoryPath, @"SeedData/CategorySeed.json");
+                using (StreamReader r = new StreamReader(categorySeedPath))
+                {
+                    string json = r.ReadToEnd();
+                    categories = JsonSerializer.Deserialize<List<Category>>(json);
+                }
+
+                return categories ?? new();
+            }
+        }
+    }
+}
---
+++ src/services/products/Products.Domain/Products/Product.cs
@@ -0,0 +1,20 @@
+﻿using Products.Domain.Base;
+using Products.Domain.Categories;
+
+namespace Products.Domain.Products
+{
+    public class Product : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+        public string Permalink { get; set; }
+        public string CoverImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string Code { get; set; }
+
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
